edd36d4e0bb4dbdadc77321498c12bc2ad4a9ace
everetr <2319774-everetr@users.noreply.gitlab.com>
2020-03-06 20:15:41 -0500

    Fix and simplify tests.

--- tests/search_tests.py
+++ tests/search_tests.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from search import CLSearch
 import sys
+sys.path.append("/PATH/TO/craigapts/craigapts")
 
-sys.path.append(r"/path/to/craigapts")
+from search import CLSearch
 
-GEO = "newjersey"
-QUERY = "'no section 8'"
-
-_all = CLSearch(GEO)
-n_all = _all.data.shape[0]
-
-_ns8 = CLSearch(GEO, QUERY)
-n_ns8 = _ns8.data.shape[0]
-
-pct_ns8 = round((n_ns8/n_all)*100, 1)
-print(f"{n_ns8:,} ({pct_ns8}%) of all {n_all:,} Craigslist ads in {GEO} contain {QUERY}.")
+nj_ns8 = CLSearch(geo="newjersey", query="'no section 8'")
+nj_ns8_df = nj_ns8.data
