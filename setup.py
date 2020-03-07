07003184ef0c73476062df49a27d436453833faf
everetr <2319774-everetr@users.noreply.gitlab.com>
2020-03-06 20:18:35 -0500

    Prep for release.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://gitlab.com/everetr/craigapts'
 EMAIL = ''
 AUTHOR = 'Everet Rummel'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '2020.2.23.1.9001'
+VERSION = '2020.3.6.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
