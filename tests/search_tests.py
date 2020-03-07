8bce53afdaf156c8fc6a21ef28b00da394d771d9
everetr <2319774-everetr@users.noreply.gitlab.com>
2020-03-07 18:40:43 -0500

    Housekeeping.

--- tests/search_tests.py
+++ tests/search_tests.py
@@ -6,6 +6,6 @@ sys.path.append("/PATH/TO/craigapts/craigapts")
 import pandas as pd
 from search import CLSearch
 
-nnj_ns8 = CLSearch(geo="newjersey", query='"bedroom"')
+nnj_ns8 = CLSearch(geo="newjersey", query='"no section 8"')
 df = nnj_ns8.data
 any(df.duplicated("post_id"))
